8d0b0cc4e0468552b36ed85a05f4ae0cba1753ba
David Walker <dwalker@calstate.edu>
2020-05-21 07:27:20 -0700

    update method

--- lib/handle_system/client.rb
+++ lib/handle_system/client.rb
@@ -25,19 +25,30 @@ module HandleSystem
     #
     # @param [String] handle    e.g., 20.500.12345/876
     # @param [String] url       the url we want to register
-    # @param [String] email     [optional] email address to add to record
-    # @param [String] hs_admin  [optional] handle administrator to add to record
+    # @param [String] email     [optional] email address
+    # @param [String] hs_admin  [optional] handle administrator
     #
     # @return [string] the new handle url at hdl.handle.net
     #
     def create(handle, url, email = nil, hs_admin = nil)
-      body = Record.new.from_values(handle, url, email, hs_admin).json
-      json = @http_client.put_it('/handles/' + handle, body)
-      @handle_base + json['handle']
+      set_record(handle, url, email, hs_admin, false)
     end
 
-    # creating and updating handle records is the same thing
-    alias update create
+    #
+    # Update an existing handle
+    #
+    # Will create a new handle if no record already exists
+    #
+    # @param [String] handle    e.g., 20.500.12345/876
+    # @param [String] url       the url we want to register
+    # @param [String] email     [optional] email address
+    # @param [String] hs_admin  [optional] handle administrator
+    #
+    # @return [string] the new handle url at hdl.handle.net
+    #
+    def update(handle, url, email = nil, hs_admin = nil)
+      set_record(handle, url, email, hs_admin, true)
+    end
 
     #
     # Return the full record for a handle
@@ -61,6 +72,27 @@ module HandleSystem
     def delete(handle)
       json = @http_client.delete_it('/handles/' + handle)
       return true if json['responseCode'] == 1
+    end
+
+    protected
+
+    #
+    # Create or update a handle
+    #
+    # @param [String] handle     e.g., 20.500.12345/876
+    # @param [String] url        the url we want to register
+    # @param [String] email      [optional] email address to add to record
+    # @param [String] hs_admin   [optional] handle administrator
+    # @param [Boolean] overwrite [optional] overwrite any existing record?
+    #                              default: false
+    #
+    # @return [string] the new handle url at hdl.handle.net
+    #
+    def set_record(handle, url, email = nil, hs_admin = nil, overwrite = false)
+      body = Record.new.from_values(handle, url, email, hs_admin).json
+      url = '/handles/' + handle + '?overwrite=' + overwrite.to_s
+      json = @http_client.put(url, body)
+      @handle_base + json['handle']
     end
   end
 end
